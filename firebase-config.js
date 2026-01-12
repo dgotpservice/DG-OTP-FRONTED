8ec08cfe50f4e54369bd52a32db4a6335faf030e
dgotpservice <dgotpservice@gmail.com>
2026-01-12 12:26:04 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// ============================
+// DG SOCIAL SERVICE - Firebase Config
+// ============================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkWf5kB35rbr6fQp5EJxwZ2bG6xzDlzZQ",
+    authDomain: "otp-service-74871.firebaseapp.com",
+    databaseURL: "https://otp-service-74871-default-rtdb.firebaseio.com"
+};
